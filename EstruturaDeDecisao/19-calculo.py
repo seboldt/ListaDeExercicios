03334c974fdd67606f2744b4e46d34d4004f681f
root <seboldtlucas@gmail.com>
2019-05-06 19:18:50 -0300

    19 20

--- EstruturaDeDecisao/19-calculo.py
+++ EstruturaDeDecisao/19-calculo.py
@@ -2,9 +2,10 @@ n = int(input('informe um numero menor que 1000 \n'))
 
 if n < 1000:
     centenas = n /100
-    print('Centenas: ',int(centenas))
+    print('Centenas:',int(centenas))
 
     dezenas = (n - (int(centenas) * 100)) / 10
-    print('Dezenas: ', int(dezenas))
+    print('Dezenas:', int(dezenas))
 
-
+    unidades = n - (int(centenas) * 100) - (int(dezenas) * 10)
+    print(f'Unidades: {unidades}')
