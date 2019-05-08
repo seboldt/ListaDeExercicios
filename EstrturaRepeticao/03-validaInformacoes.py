f35e641be56dbbb50833e7f48d16de34044155fc
Lucas Seboldt <seboldtlucas@gmail.com>
2019-05-08 11:33:50 -0300

    Estrutura de Repetição

--- EstrturaRepeticao/03-validaInformacoes.py
+++ EstrturaRepeticao/03-validaInformacoes.py
@@ -8,7 +8,34 @@ while len(nome) <= 3:
 idade = -1
 while idade < 0 or idade > 150:
     idade = int(input('Informe a idade \n'))
-    
+    if idade < 0 or idade > 150:
+        print('idade invalida')
+
+salario = -2
+
+while salario < 0:
+    salario = float(input('informe seu salario \n'))
+    if salario < 0:
+        print('salario informado é invalido')
+
+
+sexo = ''
+
+while sexo != 'M' and sexo != 'F':
+    sexo = input('Informe o sexo \nM-Masculino \nF-Feminino\n').upper()
+    if sexo != 'M' and sexo != 'F':
+        print('Sexo invalido')
+
+estado_civil = 'A'
+tipo = 'SCVD'
+
+while tipo.find(estado_civil) < 0:
+    estado_civil = input('informe estado civil \nS-Solteiro \nC-Casado \nV-Viuvo \nD-Divorciado\n').upper()
+    if tipo.find(estado_civil) < 0:
+        print('estado civil invalido')
 
 
 print(nome)
+print(salario)
+print(sexo)
+print(estado_civil)
